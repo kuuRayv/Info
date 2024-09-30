9b75e54a2271779ff64dbcd1d7b65d72a0057d5b
kuuRayv <161889221+kuuRayv@users.noreply.github.com>
2024-09-30 21:28:06 +0200

    updated square creation

--- AlgoProg/Tetris/Iteration1/test_tetris_v1.ml
+++ AlgoProg/Tetris/Iteration1/test_tetris_v1.ml
@@ -6,32 +6,30 @@ type t_param = {size_x : int ; size_y : int ; dilat : int ; origin : t_point} ;;
 
 
 let param : t_param = {size_x : int = 6 ; size_y : int = 12 ;
- dilat : int = 64 ; origin : t_point = {x = 64 ; y = 64}} ;;
+ dilat : int = 64 ; origin : t_point = {x = 65 ; y = 65}} ;;
 
 
-(* dessine un carré avec bordure, de taille dilat, à partir des coordonées d'un point dans l'espace de travail et d'une couleur*)
+(* dessine un carré avec bordure, de taille dilat, à partir
+  des coordonées d'un point dans l'espace de travail et d'une couleur*)
 let draw_point (p_param, p_point, p_color : t_param * t_point * t_color) : unit =
   set_color (p_color) ;
   fill_rect (p_point.x * p_param.dilat + p_param.origin.x, p_point.y * p_param.dilat
-  + p_param.origin.y, p_param.dilat, p_param.dilat) ;
+  + p_param.origin.y, p_param.dilat - 1, p_param.dilat - 1) ;
   set_color (black) ;
-  draw_rect (p_point.x * p_param.dilat + p_param.origin.x + 1, p_point.y * p_param.dilat
-  + p_param.origin.y + 1, p_param.dilat - 2, p_param.dilat - 2) ;
-  set_color (white) ;
   draw_rect (p_point.x * p_param.dilat + p_param.origin.x, p_point.y * p_param.dilat
-  + p_param.origin.y, p_param.dilat, p_param.dilat) ;;
+  + p_param.origin.y, p_param.dilat - 1, p_param.dilat - 1) ;;
 
 
 (* dessine une bordure noire incomplète autour de l'espace de travail *)
 let draw_frame (p_param) : unit =
   set_color (black) ;
-  fill_rect (0, 0, (p_param.size_x + 2) * p_param.dilat, p_param.dilat) ;
-  fill_rect (0, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat) ;
-  fill_rect ((p_param.size_x + 1) * p_param.dilat, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat) ;;
+  fill_rect (0, 0, (p_param.size_x + 2) * p_param.dilat + 1, p_param.dilat) ;
+  fill_rect (0, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat + 1) ;
+  fill_rect ((p_param.size_x + 1) * p_param.dilat + 1, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat + 1) ;;
 
 
 
-open_graph ((param.size_x + 2) * param.dilat, (param.size_y + 1) * param.dilat) ;;
+open_graph ((param.size_x + 2) * param.dilat + 1, (param.size_y + 1) * param.dilat + 1) ;;
 
 (* test de la fonction draw_frame *)
 draw_frame (param) ;;
@@ -44,7 +42,10 @@ draw_frame (test_param) ;; *)
 draw_point (param, {x = 0 ; y = 0}, red) ;;
 draw_point (param, {x = 1 ; y = 0}, red) ;;
 draw_point (param, {x = 0 ; y = 1}, red) ;;
+draw_point (param, {x = 1 ; y = 1}, red) ;;
 draw_point (param, {x = 3 ; y = 4}, blue) ;;
+draw_point (param, {x = 3 ; y = 3}, blue) ;;
 draw_point (param, {x = 4 ; y = 8}, blue) ;;
 draw_point (param, {x = 5 ; y = 2}, green) ;;
 draw_point (param, {x = 2 ; y = 3}, green) ;;
+draw_point (param, {x = 5 ; y = 11}, green) ;;
--- AlgoProg/Tetris/Iteration1/tetris_v1.ml
+++ AlgoProg/Tetris/Iteration1/tetris_v1.ml
@@ -6,32 +6,30 @@ type t_param = {size_x : int ; size_y : int ; dilat : int ; origin : t_point} ;;
 
 
 let param : t_param = {size_x : int = 6 ; size_y : int = 12 ;
- dilat : int = 64 ; origin : t_point = {x = 64 ; y = 64}} ;;
+ dilat : int = 64 ; origin : t_point = {x = 65 ; y = 65}} ;;
 
 
-(* dessine un carré avec bordure, de taille dilat, à partir des coordonées d'un point dans l'espace de travail et d'une couleur*)
+(* dessine un carré avec bordure, de taille dilat, à partir
+  des coordonées d'un point dans l'espace de travail et d'une couleur*)
 let draw_point (p_param, p_point, p_color : t_param * t_point * t_color) : unit =
   set_color (p_color) ;
   fill_rect (p_point.x * p_param.dilat + p_param.origin.x, p_point.y * p_param.dilat
-  + p_param.origin.y, p_param.dilat, p_param.dilat) ;
+  + p_param.origin.y, p_param.dilat - 1, p_param.dilat - 1) ;
   set_color (black) ;
-  draw_rect (p_point.x * p_param.dilat + p_param.origin.x + 1, p_point.y * p_param.dilat
-  + p_param.origin.y + 1, p_param.dilat - 2, p_param.dilat - 2) ;
-  set_color (white) ;
   draw_rect (p_point.x * p_param.dilat + p_param.origin.x, p_point.y * p_param.dilat
-  + p_param.origin.y, p_param.dilat, p_param.dilat) ;;
+  + p_param.origin.y, p_param.dilat - 1, p_param.dilat - 1) ;;
 
 
 (* dessine une bordure noire incomplète autour de l'espace de travail *)
 let draw_frame (p_param) : unit =
   set_color (black) ;
-  fill_rect (0, 0, (p_param.size_x + 2) * p_param.dilat, p_param.dilat) ;
-  fill_rect (0, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat) ;
-  fill_rect ((p_param.size_x + 1) * p_param.dilat, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat) ;;
+  fill_rect (0, 0, (p_param.size_x + 2) * p_param.dilat + 1, p_param.dilat) ;
+  fill_rect (0, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat + 1) ;
+  fill_rect ((p_param.size_x + 1) * p_param.dilat + 1, 0, p_param.dilat, (p_param.size_y + 1) * p_param.dilat + 1) ;;
 
 
 
-open_graph ((param.size_x + 2) * param.dilat, (param.size_y + 1) * param.dilat) ;;
+open_graph ((param.size_x + 2) * param.dilat + 1, (param.size_y + 1) * param.dilat + 1) ;;
 
 draw_frame (param) ;;
 
